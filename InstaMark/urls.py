f840a6b8fad22c2c1f94adf8ac8884f8071b5159
SurajKumar <surajramagiri@gmail.com>
2024-12-11 20:07:07 +0530

    first commit

---
+++ InstaMark/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for InstaMark project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('settings/',views.settings,name='settings'),
+    path('attendance/',views.attendance ,name='attendance'),
+    path('user_management/',views.user_management,name='user_management'),
+    path('data_capture/',views.DataCapture, name='data_capture'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('',views.home,name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/',views.user_logout,name='logout'),
+    path('login/',views.user_login, name='login'),
+    path('dashboard/',views.dashboard,name='dashboard'),
+    path("admin/", admin.site.urls),
+]
---
+++ InstaMark/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required,user_passes_test
+from . import forms
+
+def settings(request):
+    return render(request,'settings.html')
+
+def attendance(request):
+    return render(request,'attendance.html')
+
+def user_management(request):
+    return render(request,'user_management.html')
+
+def DataCapture():
+    return render(request,'DataCapture.html')
+
+# Function to check if user is admin
+def is_admin(user):
+    return user.is_superuser
+
+# Dashboard view
+@user_passes_test(is_admin)
+def admin_dashboard(request):
+    user=request.user.username
+    return render(request, 'admin_dashboard.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            messages.success(request, 'Account created successfully!')
+            login(request,user)
+            return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request,'dashboard.html')
+
+def user_login(request):
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return redirect('admin_dashboard')
+        return redirect('dashboard')         # Redirect to the home page if the user is logged in
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                if user.is_superuser:
+                    return redirect('admin_dashboard')
+                else:
+                    return redirect('dashboard')  # Redirect to a dashboard page after successful login
+            else:
+                form.add_error(None, 'Invalid username or password')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
